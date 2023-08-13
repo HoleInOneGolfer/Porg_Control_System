5e09b19248a69021150eb334dd2e167b015c5a3e
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-08-12 19:54:48 -0500

    mmap

--- src/Dome.h
+++ src/Dome.h
@@ -4,25 +4,40 @@
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
 
+#include <map>
+
 // =============== CONFIG ===============
 // Servo Pins
-byte SIDE_PINS[6] = {0, 0, 0, 0, 0, 0};
-byte PIE_PINS[5] = {0, 0, 0, 0, 0};
-byte HV1_PIN = 0;
-byte HH1_PIN = 0;
-byte HV2_PIN = 0;
-byte HH2_PIN = 0;
-byte HV3_PIN = 0;
-byte HH3_PIN = 0;
+const std::map<String, byte> DOME_SERVO_PINS = {
+    {"HV1", 0},
+    {"HH1", 0},
+    {"HV2", 0},
+    {"HH2", 0},
+    {"HV3", 0},
+    {"HH3", 0},
+    {"SIDE1", 0},
+    {"SIDE2", 0},
+    {"SIDE3", 0},
+    {"SIDE4", 0},
+    {"SIDE5", 0},
+    {"SIDE6", 0},
+    {"PIE1", 0},
+    {"PIE2", 0},
+    {"PIE3", 0},
+    {"PIE4", 0},
+    {"PIE5", 0},
+};
 
 // Light Pins
-byte HL1_PIN = 0;
-byte HL2_PIN = 0;
-byte HL3_PIN = 0;
-byte RPSI_PIN = 0;
-byte FPSI_PIN = 0;
-byte FLD_PIN = 0;
-byte RLD_PIN = 0;
+const std::map<String, byte> DOME_LIGHT_PINS = {
+    {"HL1", 0},
+    {"HL2", 0},
+    {"HL3", 0},
+    {"RPSI", 0},
+    {"FPSI", 0},
+    {"FLD", 0},
+    {"RLD", 0},
+};
 
 // Light Strip Counts
 const byte HL1_CNT = 7;
@@ -33,34 +48,27 @@ const byte FPSI_CNT = 21;
 const byte FLD_CNT = 26 * 4;
 const byte RLD_CNT = 2 * (5 * 9);
 
-Servo SIDE_SERVOS[6], PIE_SERVOS[5], HV1_SERVO, HH1_SERVO, HV2_SERVO, HH2_SERVO, HV3_SERVO, HH3_SERVO;
-Adafruit_NeoPixel HL1_STRIP, HL2_STRIP, HL3_STRIP, RPSI_STRIP, FPSI_STRIP, FLD_STRIP, RLD_STRIP;
+// =============== OBJECTS ===============
+std::map<String, Servo> DOME_SERVOS;
+std::map<String, Adafruit_NeoPixel> DOME_LIGHTS;
 
 void initDome()
 {
-    // assign pins
-    for (byte i = 0; i < 6; i++)
+    // Initialize Servos
+    for (auto const &[name, pin] : DOME_SERVO_PINS)
     {
-        SIDE_SERVOS[i].attach(SIDE_PINS[i]);
+        Servo servo;
+        servo.attach(pin);
+        DOME_SERVOS[name] = servo;
     }
-    for (byte i = 0; i < 5; i++)
-    {
-        PIE_SERVOS[i].attach(PIE_PINS[i]);
-    }
-    HV1_SERVO.attach(HV1_PIN);
-    HH1_SERVO.attach(HH1_PIN);
-    HV2_SERVO.attach(HV2_PIN);
-    HH2_SERVO.attach(HH2_PIN);
-    HV3_SERVO.attach(HV3_PIN);
-    HH3_SERVO.attach(HH3_PIN);
 
-    HL1_STRIP = Adafruit_NeoPixel(HL1_CNT, HL1_PIN, NEO_GRB + NEO_KHZ800);
-    HL2_STRIP = Adafruit_NeoPixel(HL2_CNT, HL2_PIN, NEO_GRB + NEO_KHZ800);
-    HL3_STRIP = Adafruit_NeoPixel(HL3_CNT, HL3_PIN, NEO_GRB + NEO_KHZ800);
-    RPSI_STRIP = Adafruit_NeoPixel(RPSI_CNT, RPSI_PIN, NEO_GRB + NEO_KHZ800);
-    FPSI_STRIP = Adafruit_NeoPixel(FPSI_CNT, FPSI_PIN, NEO_GRB + NEO_KHZ800);
-    FLD_STRIP = Adafruit_NeoPixel(FLD_CNT, FLD_PIN, NEO_GRB + NEO_KHZ800);
-    RLD_STRIP = Adafruit_NeoPixel(RLD_CNT, RLD_PIN, NEO_GRB + NEO_KHZ800);
+    // Initialize Lights
+    for (auto const &[name, pin] : DOME_LIGHT_PINS)
+    {
+        Adafruit_NeoPixel strip;
+        strip = Adafruit_NeoPixel(0, pin, NEO_GRB + NEO_KHZ800);
+        DOME_LIGHTS[name] = strip;
+    }
 }
 
 #endif
